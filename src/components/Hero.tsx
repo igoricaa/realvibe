51fa6245e1545961618b774ecf8ecd80bea7d4af
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-10-02 22:56:35 +0200

    hero animation fine tuning, text, scroll down

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,9 +7,11 @@ import heroImage from '../../public/hero-image.png';
 import { useScroll, motion, useTransform } from 'framer-motion';
 import styles from './Hero.module.scss';
 import { useRef, useEffect, useState } from 'react';
+import wireframe from '../../public/wireframe.svg';
 
 const Hero = () => {
   const heroRef = useRef<HTMLDivElement>(null);
+  const imageTextRef = useRef<HTMLDivElement>(null);
   const [viewportHeight, setViewportHeight] = useState(0);
   const [viewportWidth, setViewportWidth] = useState(0);
 
@@ -39,16 +41,36 @@ const Hero = () => {
     { clamp: true }
   );
 
-  const scale = useTransform(
+  const opacityFast = useTransform(scrollYProgress, [0, 0.1], [1, 0], {
+    clamp: true,
+  });
+
+  const opacitySlow = useTransform(scrollYProgress, [0, 0.2], [1, 0], {
+    clamp: true,
+  });
+
+  const width = useTransform(
     animationProgress,
     [0, 1],
-    [1, Math.max((viewportWidth - 64) / 638, (viewportHeight - 64) / 350)]
+    [638, viewportWidth - 64]
   );
 
-  const yPosition = useTransform(
+  const height = useTransform(
     animationProgress,
     [0, 1],
-    [0, viewportHeight + 850]
+    [350, viewportHeight - 64]
+  );
+
+  const yPositionUp = useTransform(
+    animationProgress,
+    [0, 1],
+    [0, -viewportHeight]
+  );
+
+  const yPositionDown = useTransform(
+    animationProgress,
+    [0, 1],
+    [0, viewportHeight / 2 - 150]
   );
 
   const xPosition = useTransform(animationProgress, [0, 1], [0, 100]);
@@ -57,7 +79,10 @@ const Hero = () => {
 
   return (
     <div className={styles.hero} ref={heroRef}>
-      <div className={styles.hero__content}>
+      <motion.div
+        className={styles.hero__content}
+        style={{ opacity: opacityFast, y: yPositionUp }}
+      >
         <h1 className={styles.hero__title}>{t('title')}</h1>
         <p className={styles.hero__subtitle}>
           {t('subtitle.one')} <span>{t('subtitle.two')}</span>{' '}
@@ -66,18 +91,47 @@ const Hero = () => {
         <Link href='/contact' className={styles.hero__button}>
           {t('cta')}
         </Link>
-      </div>
+      </motion.div>
 
       <motion.div
         className={styles.hero__image}
         style={{
-          scale,
-          y: yPosition,
+          width,
+          height,
+          y: yPositionDown,
           x: xPosition,
         }}
       >
-        <Image src={heroImage} alt='Hero' fill />
+        <motion.div
+          ref={imageTextRef}
+          className={styles.hero__image__text}
+          style={{ opacity: opacityFast }}
+        >
+          <h6>REALVIBE DEMO REEL</h6>
+          <p>
+            RealVibe excels at organizing professional
+            <br />
+            business events that inspire, engage, and
+            <br />
+            leave a lasting impression.
+          </p>
+        </motion.div>
+        <Image src={heroImage} alt='RealVibe hero image' fill />
       </motion.div>
+
+      <motion.div
+        className={styles.scrollDown}
+        style={{ opacity: opacitySlow }}
+      >
+        <p>Scroll Down</p>
+      </motion.div>
+      {/* <Image
+        className={styles.wireframe}
+        src={wireframe}
+        alt='RealVibe wireframe'
+        width={343}
+        height={343}
+      /> */}
     </div>
   );
 };
