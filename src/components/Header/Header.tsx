5eca2eaf6dd841ed5b331571e4f95fceda11d0e0
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-10-07 12:55:16 +0200

    header responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.scss';
 import logo from '@/../public/realvibe-logo.svg';
 import { Link } from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
-import sideareaBurger from '@/../public/sidearea-burger.svg';
+import burger from '@/../public/burger.svg';
 
 const Header = () => {
   const t = useTranslations('nav');
@@ -19,28 +19,38 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__logoWrapper}>
-        <Image
-          src={logo}
-          alt='Real Vibe Production logo'
-          width={137}
-          height={91}
-        />
+        <Image src={logo} alt='Real Vibe Production logo' fill priority />
       </div>
       <nav className={styles.header__nav}>
         <ul className={styles.header__nav__desktopMenu}>
           {routes.map((route) => (
-            <li className={styles.header__nav__desktopMenu__item} key={route.href}>
+            <li
+              className={styles.header__nav__desktopMenu__item}
+              key={route.href}
+            >
               <Link href={route.href as any}>{route.label}</Link>
             </li>
           ))}
         </ul>
         <div className={styles.header__nav__side__area}>
-          <Image
-            src={sideareaBurger}
-            alt='Sidearea icon'
-            width={20}
-            height={40}
-          />
+          <Image src={burger} alt='Burger icon' fill priority />
+        </div>
+      </nav>
+      <nav className={styles.header__nav__mobile}>
+        <div className={styles.header__nav__mobile__menuWrapper}>
+          <ul className={styles.header__nav__mobile__menu}>
+            {routes.map((route) => (
+              <li
+                className={styles.header__nav__mobile__menu__item}
+                key={route.href}
+              >
+                <Link href={route.href as any}>{route.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.header__nav__mobile__burger}>
+          <Image src={burger} alt='Burger icon' fill priority />
         </div>
       </nav>
     </header>
