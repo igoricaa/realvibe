8d3af2e3700d8e8b3e8138eb21acfd530997b586
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-10-08 10:02:46 +0200

    about us translate animation framer to css

--- src/components/About/HorizontalSlider/AboutSlider.tsx
+++ src/components/About/HorizontalSlider/AboutSlider.tsx
@@ -5,9 +5,10 @@ import styles from './AboutSlider.module.scss';
 import { Link } from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
 import { useScroll, useTransform, motion } from 'framer-motion';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import Button from '@/components/UI/Button';
 import useMediaQuery from '@/hooks/useMediaQuery';
+import { useInView } from 'framer-motion';
 
 type CompanyValue = {
   title: string;
@@ -55,55 +56,42 @@ const AboutSlider = () => {
           style={{ x: isMobile ? 0 : x }}
           className={styles.about__slider__content}
         >
-          {companyValues.map((value, index) => (
-            <motion.div
-              key={index}
-              className={styles.about__slider__item}
-              initial={{ opacity: 0, transform: 'translateY(300px)' }}
-              whileInView={{ opacity: 1, transform: 'translateY(0)' }}
-              transition={{ duration: 1, ease: 'easeOut' }}
-              // viewport={{ once: true }}
+          {companyValues.map((value, index) => {
+            const ref = useRef(null);
+            const isInView = useInView(ref, { once: true });
 
-              // initial={{ opacity: 0, y: 300 }}
-              // whileInView={{ opacity: 1, y: 0 }}
-              // viewport={{ once: true}}
-              // transition={{ duration: 1, ease: 'easeOut' }}
-            >
-              <div className={styles.about__slider__item__image}>
-                <Image
-                  src={value.image}
-                  alt={value.title}
-                  fill
-                  sizes='(max-width: 768px) 75vw, 448px'
-                />
-              </div>
-              <div className={styles.about__slider__item__content}>
-                <div className={styles.about__slider__item__content__icon}>
-                  <Image src={value.icon} alt={value.title} fill />
+            return (
+              <div
+                key={index}
+                ref={ref}
+                className={`${styles.about__slider__item} ${
+                  isInView ? styles.inView : ''
+                }`}
+              >
+                <div className={styles.about__slider__item__image}>
+                  <Image
+                    src={value.image}
+                    alt={value.title}
+                    fill
+                    sizes='(max-width: 768px) 75vw, 448px'
+                  />
                 </div>
-                <h4>{t('our-mission.title')}</h4>
-                <p>{t('our-mission.description')}</p>
-                <Button variant='secondary'>
-                  <Link href={'/contact'}>{t('our-mission.cta')}</Link>
-                </Button>
+                <div className={styles.about__slider__item__content}>
+                  <div className={styles.about__slider__item__content__icon}>
+                    <Image src={value.icon} alt={value.title} fill />
+                  </div>
+                  <h4>{t('our-mission.title')}</h4>
+                  <p>{t('our-mission.description')}</p>
+                  <Button variant='secondary'>
+                    <Link href={'/contact'}>{t('our-mission.cta')}</Link>
+                  </Button>
+                </div>
               </div>
-            </motion.div>
-          ))}
+            );
+          })}
         </motion.div>
       </div>
     </section>
-
-    // return (
-    //   <section  className="relative h-[300vh] bg-neutral-900">
-    //     <div className="sticky top-0 flex h-screen items-center overflow-hidden">
-    //       <motion.div style={{ x }} className="flex gap-4">
-    //         {cards.map((card) => {
-    //           return <Card card={card} key={card.id} />;
-    //         })}
-    //       </motion.div>
-    //     </div>
-    //   </section>
-    // );
   );
 };
 
