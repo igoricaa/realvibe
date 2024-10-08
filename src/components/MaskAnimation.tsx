5f6b8c6ece47a4990ab4bf7b7d778b45a8dd259c
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-10-08 08:21:28 +0200

    mask animation optimization

--- src/components/MaskAnimation.tsx
+++ src/components/MaskAnimation.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { motion, useScroll, useTransform } from 'framer-motion';
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+} from 'framer-motion';
 import styles from './MaskAnimation.module.scss';
 
 const MaskAnimation: React.FC = () => {
@@ -26,74 +32,44 @@ const MaskAnimation: React.FC = () => {
 
   const initialMaskSize =
     Math.min(viewportSize.width, viewportSize.height) * 0.1;
-
-  const maskSize = useTransform(
-    scrollYProgress,
-    [0, 1],
-    [
-      initialMaskSize,
-      Math.sqrt(
-        Math.pow(viewportSize.width, 2) + Math.pow(viewportSize.height, 2)
-      ),
-    ]
+  const maxMaskSize = Math.sqrt(
+    Math.pow(viewportSize.width, 2) + Math.pow(viewportSize.height, 2)
   );
 
-  const imageScale = useTransform(scrollYProgress, [0, 1], [1, 1]);
+  const smoothProgress = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
-  const imageBrightness = useTransform(scrollYProgress, [0, 1], [100, 150]);
+  const maskSize = useTransform(
+    smoothProgress,
+    [0, 1],
+    [initialMaskSize, maxMaskSize]
+  );
+  const clipPathValue = useMotionValue(
+    `circle(${initialMaskSize}px at 50% 50%)`
+  );
+
+  const imageBrightness = useTransform(smoothProgress, [0, 1], [100, 150]);
+
+  useEffect(() => {
+    const unsubscribe = maskSize.onChange((v) => {
+      clipPathValue.set(`circle(${v}px at 50% 50%)`);
+    });
+    return unsubscribe;
+  }, [maskSize, clipPathValue]);
 
   return (
     <div ref={containerRef} className={styles.mask}>
       <div className={styles.content}>
-        <svg
+        <motion.div
           className={styles.content__img}
-          width='100%'
-          height='100%'
-          version='1.1'
-          xmlns='http://www.w3.org/2000/svg'
-          xmlnsXlink='http://www.w3.org/1999/xlink'
-          viewBox={`0 0 ${viewportSize.width} ${viewportSize.height}`}
-          preserveAspectRatio='xMidYMid slice'
-        >
-          <defs>
-            <filter id='displacementFilter4'>
-              <feTurbulence
-                type='fractalNoise'
-                baseFrequency='0.5'
-                numOctaves='1'
-                result='noise'
-              />
-              <feDisplacementMap
-                in='SourceGraphic'
-                in2='noise'
-                scale='50'
-                xChannelSelector='R'
-                yChannelSelector='G'
-              />
-            </filter>
-            <mask id='circleMask4'>
-              <motion.circle
-                cx='50%'
-                cy='50%'
-                fill='white'
-                className={styles.mask}
-                style={{ filter: 'url(#displacementFilter4)' }}
-                r={maskSize}
-              />
-            </mask>
-          </defs>
-          <motion.image
-            xlinkHref='masked-image.png'
-            width='100vw'
-            height='100vh'
-            mask='url(#circleMask4)'
-            style={{
-              scale: imageScale,
-              filter: `brightness(${imageBrightness}%)`,
-              transformOrigin: '50% 50%',
-            }}
-          />
-        </svg>
+          style={{
+            filter: `brightness(${imageBrightness}%)`,
+            clipPath: clipPathValue,
+          }}
+        />
       </div>
     </div>
   );
