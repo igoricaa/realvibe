6e5813f55ff9598c9a9182b6b066ebb30ef5c8a7
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-10-09 12:19:55 +0200

    theme switcher styling

--- src/components/UI/ThemeSwitcher.tsx
+++ src/components/UI/ThemeSwitcher.tsx
@@ -2,8 +2,11 @@
 
 import { useTheme } from 'next-themes';
 import { useState, useEffect } from 'react';
+import styles from './ThemeSwitcher.module.scss';
+import DarkJarIcon from './icons/DarkJarIcon';
+import LightJarIcon from './icons/LightJarIcon';
 
-const ThemeSwitcher = () => {
+export default function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
@@ -15,11 +18,31 @@ const ThemeSwitcher = () => {
     return null;
   }
 
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
   return (
-    <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-      Switch to {theme === 'dark' ? 'light' : 'dark'} mode
+    <button
+      onClick={toggleTheme}
+      className={styles.switcher}
+      aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} theme`}
+    >
+      <span
+        className={`${styles.icon} ${theme === 'light' ? styles.active : ''}`}
+      >
+        <LightJarIcon />
+      </span>
+      <span
+        className={`${styles.icon} ${theme === 'dark' ? styles.active : ''}`}
+      >
+        <DarkJarIcon />
+      </span>
+      <span
+        className={`${styles.slider} ${
+          theme === 'dark' ? styles.sliderRight : ''
+        }`}
+      />
     </button>
   );
-};
-
-export default ThemeSwitcher;
+}
