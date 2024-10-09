29e850461e00defe83857bc7ec8205eca1f8b955
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-10-09 09:45:53 +0200

    hopefully final fix

--- src/components/Header/SideArea.tsx
+++ src/components/Header/SideArea.tsx
@@ -9,12 +9,10 @@ import { services, Service } from '@/data';
 import Button from '../UI/Button';
 import { useState } from 'react';
 import BurgerIcon from '../UI/icons/BurgerIcon';
-import useMediaQuery from '@/hooks/useMediaQuery';
 
 const SideArea = () => {
   const t = useTranslations('nav.sidearea');
   const [isOpen, setIsOpen] = useState(false);
-  const isMobile = useMediaQuery('(max-width: 768px)');
 
   const handleClick = () => {
     setIsOpen(!isOpen);
@@ -22,97 +20,87 @@ const SideArea = () => {
   };
 
   return (
-    <>
-      {!isMobile && (
-        <section className={styles.sidearea__wrapper}>
-          <BurgerIcon onClick={handleClick} />
-          <div
-            className={`${styles.overlay} ${isOpen ? styles.open : ''}`}
-            data-lenis-prevent
-          ></div>
-          <div
-            id='sidearea'
-            className={`${styles.sidearea} ${isOpen ? styles.open : ''}`}
-            data-lenis-prevent
-          >
-            <XIcon color='#fff' onClick={handleClick} />
-            <div className={styles.sidearea__contentWrapper}>
-              <h4 className={styles.sidearea__title}>
-                {t('title.one')} <span>{t('title.two')}</span>
-              </h4>
-              <Image
-                src={featuredProject}
-                alt='Real Vibe Featured Project'
-                width={532}
-                height={342}
-                className={styles.sidearea__image}
-              />
-              <div className={styles.sidearea__content}>
-                <div className={styles.sidearea__services}>
-                  <h5 className={styles.sidearea__services__title}>
-                    {t('services.title.one')}{' '}
-                    <span>{t('services.title.two')}</span>
-                  </h5>
-                  <ul className={styles.sidearea__services__list}>
-                    {services.map((service: Service, index: number) => (
-                      <li
-                        key={service.title}
-                        className={`${styles.sidearea__services__list__item} ${
-                          styles[
-                            `sidearea__services__list__item__${service.title}`
-                          ]
-                        }`}
-                      >
-                        <a href=''>
-                          <i>
-                            <Image
-                              src={service.icon}
-                              alt={t(`services.service-${index + 1}.title`)}
-                              width={24}
-                              height={24}
-                            />
-                          </i>
-                          <span>
-                            {t(`services.service-${index + 1}.title`)}
-                          </span>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className={styles.sidearea__socials}>
-                  <h5 className={styles.sidearea__socials__title}>
-                    {t('socials.title.one')}{' '}
-                    <span>{t('socials.title.two')}</span>
-                  </h5>
-                  <ul className={styles.sidearea__socials__list}>
-                    <li className={styles.sidearea__socials__list__item}>
-                      <a
-                        href='https://www.instagram.com/realvibestudio/'
-                        target='_blank'
-                      >
-                        Instagram
-                      </a>
-                    </li>
-                    <li className={styles.sidearea__socials__list__item}>
-                      <a
-                        href='https://www.tiktok.com/@realvibestudio'
-                        target='_blank'
-                      >
-                        TikTok
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+    <section className={styles.sidearea__wrapper}>
+      <BurgerIcon onClick={handleClick} />
+      <div
+        className={`${styles.overlay} ${isOpen ? styles.open : ''}`}
+        data-lenis-prevent
+      ></div>
+      <div
+        id='sidearea'
+        className={`${styles.sidearea} ${isOpen ? styles.open : ''}`}
+        data-lenis-prevent
+      >
+        <XIcon color='#fff' onClick={handleClick} />
+        <div className={styles.sidearea__contentWrapper}>
+          <h4 className={styles.sidearea__title}>
+            {t('title.one')} <span>{t('title.two')}</span>
+          </h4>
+          <Image
+            src={featuredProject}
+            alt='Real Vibe Featured Project'
+            width={532}
+            height={342}
+            className={styles.sidearea__image}
+          />
+          <div className={styles.sidearea__content}>
+            <div className={styles.sidearea__services}>
+              <h5 className={styles.sidearea__services__title}>
+                {t('services.title.one')} <span>{t('services.title.two')}</span>
+              </h5>
+              <ul className={styles.sidearea__services__list}>
+                {services.map((service: Service, index: number) => (
+                  <li
+                    key={service.title}
+                    className={`${styles.sidearea__services__list__item} ${
+                      styles[`sidearea__services__list__item__${service.title}`]
+                    }`}
+                  >
+                    <a href=''>
+                      <i>
+                        <Image
+                          src={service.icon}
+                          alt={t(`services.service-${index + 1}.title`)}
+                          width={24}
+                          height={24}
+                        />
+                      </i>
+                      <span>{t(`services.service-${index + 1}.title`)}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
-            <Button>
-              <a href='/contact'>{t('cta')}</a>
-            </Button>
+            <div className={styles.sidearea__socials}>
+              <h5 className={styles.sidearea__socials__title}>
+                {t('socials.title.one')} <span>{t('socials.title.two')}</span>
+              </h5>
+              <ul className={styles.sidearea__socials__list}>
+                <li className={styles.sidearea__socials__list__item}>
+                  <a
+                    href='https://www.instagram.com/realvibestudio/'
+                    target='_blank'
+                  >
+                    Instagram
+                  </a>
+                </li>
+                <li className={styles.sidearea__socials__list__item}>
+                  <a
+                    href='https://www.tiktok.com/@realvibestudio'
+                    target='_blank'
+                  >
+                    TikTok
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </section>
-      )}
-    </>
+        </div>
+        <Button>
+          <a href='/contact'>{t('cta')}</a>
+        </Button>
+      </div>
+    </section>
   );
 };
 
