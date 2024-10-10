3b25ba859cde706b3dff81fd66e89379a1be90f4
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-10-10 15:12:27 +0200

    cursor opacity

--- src/components/UI/Cursor.tsx
+++ src/components/UI/Cursor.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './Cursor.module.scss';
 import { motion, useMotionValue, useSpring } from 'framer-motion';
 import CursorIcon from './CursorIcon';
@@ -23,25 +23,34 @@ export default function Cursor({}) {
     y: useSpring(ball.y, smoothOptions),
   };
 
+  const [isMoving, setIsMoving] = useState(true);
+  let moveTimer: NodeJS.Timeout;
+
   const manageMouseMove = (e: any) => {
     const { clientX, clientY } = e;
     mouse.x.set(clientX - cursorSize / 2);
     mouse.y.set(clientY - cursorSize / 2);
     ball.x.set(clientX - ballSize / 2);
     ball.y.set(clientY - ballSize / 2);
+
+    // Reset the timer and set isMoving to true
+    setIsMoving(true);
+    clearTimeout(moveTimer);
+    moveTimer = setTimeout(() => setIsMoving(false), 100); // Adjust timeout as needed
   };
 
   useEffect(() => {
     window.addEventListener('mousemove', manageMouseMove);
     return () => {
       window.removeEventListener('mousemove', manageMouseMove);
+      clearTimeout(moveTimer);
     };
   }, []);
 
   return (
     <div className={styles.cursorContainer}>
       <motion.div
-        className={styles.cursorIcon}
+        className={`${styles.cursorIcon}`}
         style={{
           left: mouse.x,
           top: mouse.y,
@@ -54,7 +63,7 @@ export default function Cursor({}) {
           left: smoothBall.x,
           top: smoothBall.y,
         }}
-        className={styles.cursorBall}
+        className={`${styles.cursorBall} ${isMoving ? '' : styles.notMoving}`}
       ></motion.div>
     </div>
   );
