375acacb5867b9d5208cbcd757632c058350bd94
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-12-09 14:50:29 +0100

    not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,31 @@
-import Link from 'next/link';
+import Button from '@/components/UI/Button';
+import styles from './NotFound.module.scss';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href='/'>Return Home</Link>
-    </div>
+    <main className={styles.notFound}>
+      <div
+        className='absolute inset-0 opacity-10'
+        style={{
+          backgroundImage: `linear-gradient(#333 1px, transparent 1px),
+                           linear-gradient(90deg, #333 1px, transparent 1px)`,
+          backgroundSize: '50px 50px',
+        }}
+      />
+
+      <div className={styles.notFound__content}>
+        <div className={styles.notFound__content__title}>404</div>
+
+        <h1 className={styles.notFound__content__subtitle}>Page Not Found</h1>
+
+        <p className={styles.notFound__content__description}>
+          The page you're looking for doesn't exist or has been moved.
+        </p>
+
+        <Button href='/' className='notFound'>
+          <span>Return Home</span>
+        </Button>
+      </div>
+    </main>
   );
 }
